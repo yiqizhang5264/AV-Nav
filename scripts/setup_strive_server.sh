c349eb39178d695cb918fb5165f20b4010fc34f9
yiqizhang5264 <1751816762@qq.com>
2026-09-16 17:17:29 +0800

    Build Habitat Sim headlessly with modern CMake

--- scripts/setup_strive_server.sh
+++ scripts/setup_strive_server.sh
@@ -44,7 +44,10 @@ if [[ ! -d "$DEPS_ROOT/habitat-sim/.git" ]]; then
   git clone --branch release/v0.3.2 https://github.com/zwandering/habitat-sim.git "$DEPS_ROOT/habitat-sim"
 fi
 test "$(git -C "$DEPS_ROOT/habitat-sim" rev-parse HEAD)" = "$HABITAT_SIM_COMMIT"
-"$PYTHON" -m pip install "$DEPS_ROOT/habitat-sim"
+"$PYTHON" -m pip install cmake ninja
+HEADLESS=1 WITH_CUDA=1 \
+  CMAKE_ARGS="${CMAKE_ARGS:-} -DCMAKE_POLICY_VERSION_MINIMUM=3.5" \
+  "$PYTHON" -m pip install --no-build-isolation "$DEPS_ROOT/habitat-sim"
 
 if [[ ! -d "$DEPS_ROOT/habitat-lab/.git" ]]; then
   git clone --branch release/v0.3.2 https://github.com/zwandering/habitat-lab.git "$DEPS_ROOT/habitat-lab"
